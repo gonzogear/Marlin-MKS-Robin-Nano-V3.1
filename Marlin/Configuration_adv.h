1a84e63cb0bf49515063f2705db39fe77b9bd4cc
GonzoGear <gonzofestivalgear@gmail.com>
2022-06-15 16:17:00 -0400

    1

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -4478,10 +4478,10 @@
 //
 // M43 - display pin status, toggle pins, watch pins, watch endstops & toggle LED, test servo probe
 //
-#define PINS_DEBUGGING
+//#define PINS_DEBUGGING
 
 // Enable Marlin dev mode which adds some special commands
-#define MARLIN_DEV_MODE
+//#define MARLIN_DEV_MODE
 
 #if ENABLED(MARLIN_DEV_MODE)
   /**
